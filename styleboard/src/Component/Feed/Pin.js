59cd6dd2c5283d7ac6b93778c90843cbe4b52818
Laksita <laksita.prasanna@gmail.com>
2024-12-02 20:29:15 -0500

    Pin component and css

---
+++ styleboard/src/Component/Feed/Pin.js
@@ -0,0 +1,16 @@
+import './Pin.css';
+import React from "react";
+
+function Pin({pinSize, imgSrc}) {
+    return (
+        <div className={`pin ${pinSize}`}>
+            <img
+                className='mainPic' 
+                src={imgSrc}
+                alt=""
+            />
+        </div>
+    );
+}
+
+export default Pin;
