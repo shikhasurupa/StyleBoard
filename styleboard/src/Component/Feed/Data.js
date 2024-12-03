165268e0d960534342abbfbb5138a2afea18a6d9
Laksita <laksita.prasanna@gmail.com>
2024-12-02 22:37:21 -0500

    updated data.js to use 'our' images

--- styleboard/src/Component/Feed/Data.js
+++ styleboard/src/Component/Feed/Data.js
@@ -2,242 +2,212 @@ const Data = [
     {
       id: 1,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/12/01/17/28/branch-6838719_960_720.jpg",
-      name: "Vladvictoria",
-      link: "https://pixabay.com/photos/branch-ornaments-lights-tree-6838719/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Kids/4193.jpg?raw=true",
+      type: "kids",
       size: "small",
     },
     {
       id: 2,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/12/05/21/39/christmas-balls-6848782_960_720.jpg",
-      name: "Mammiya",
-      link: "https://pixabay.com/photos/christmas-balls-christmas-advent-6848782/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Kids/41755.jpg?raw=true",
+      type: "kids",
       size: "medium",
     },
     {
       id: 3,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2020/07/29/03/58/child-5446839_960_720.jpg",
-      name: "Zhangliams",
-      link: "https://pixabay.com/photos/child-boy-run-alley-street-plants-5446839/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Kids/43590.jpg?raw=true",
+      type: "kids",
       size: "large",
     },
     {
       id: 4,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/11/14/09/14/cat-1822979_960_720.jpg",
-      name: "Mshyun",
-      link: "https://pixabay.com/photos/cat-christmas-tree-ornaments-1822979/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Kids/43575.jpg?raw=true",
+      type: "kids",
       size: "medium",
     },
     {
       id: 5,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/11/02/10/46/lemur-6762935_960_720.jpg",
-      name: "Jansheldan",
-      link: "https://pixabay.com/photos/lemur-animal-wilderness-nature-6762935/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Kids/43589.jpg?raw=true",
+      type: "kids",
       size: "large",
     },
     {
       id: 6,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/10/13/07/43/couple-6705694_960_720.jpg",
-      name: "Jupilu",
-      link: "https://pixabay.com/photos/couple-sundown-field-sunset-6705694/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Kids/43593.jpg?raw=true",
+      type: "kids",
       size: "small",
     },
     {
       id: 7,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567_960_720.jpg",
-      name: "Stergo",
-      link: "https://pixabay.com/photos/fantasy-butterflies-mushrooms-2049567/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Kids/4186.jpg?raw=true",
+      type: "kids",
       size: "medium",
     },
     {
       id: 8,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_960_720.jpg",
-      name: "Homecare",
-      link: "https://pixabay.com/photos/bird-fluttering-berries-wings-tit-1045954/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Kids/4214.jpg?raw=true",
+      type: "kids",
       size: "large",
     },
     {
       id: 9,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/11/26/20/44/lantern-6826687_960_720.jpg",
-      name: "Lasrs Nissen",
-      link: "https://pixabay.com/photos/lantern-sea-sky-beach-water-6826687/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Kids/43586.jpg?raw=true",
+      type: "kids",
       size: "medium",
     },
     {
       id: 10,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/11/30/17/06/tree-6835828_960_720.jpg",
-      name: "Jell Wllington",
-      link: "https://pixabay.com/photos/tree-snow-winter-christmas-branch-6835828/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Kids/43588.jpg?raw=true",
+      type: "kids",
       size: "medium",
     },
     {
       id: 11,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg",
-      name: "Chraecker",
-      link: "https://pixabay.com/photos/owl-bird-animal-bird-of-prey-50267/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Men/1769.jpg?raw=true",
+      type: "men",
       size: "small",
     },
     {
       id: 12,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2017/06/07/10/47/elephant-2380009_960_720.jpg",
-      name: "Comfreak",
-      link: "https://pixabay.com/photos/elephant-watering-hole-baby-elephant-2380009/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Men/1775.jpg?raw=true",
+      type: "men",
       size: "medium",
     },
     {
       id: 13,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533_960_720.jpg",
-      name: "Sasinet",
-      link: "https://pixabay.com/photos/woman-kid-rain-leaf-umbrella-1807533/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Men/1907.jpg?raw=true",
+      type: "men",
       size: "large",
     },
     {
       id: 14,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2019/08/19/07/45/dog-4415649_960_720.jpg",
-      name: "Hauad",
-      link: "https://pixabay.com/photos/dog-corgi-pets-cute-rain-4415649/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Men/1911.jpg?raw=true",
+      type: "men",
       size: "medium",
     },
     {
       id: 15,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2017/01/28/02/24/japan-2014619_960_720.jpg",
-      name: "Chau min",
-      link: "https://pixabay.com/photos/japan-osaka-night-asia-landmark-2014619/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Men/1957.jpg?raw=true",
+      type: "men",
       size: "large",
     },
     {
       id: 16,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2015/10/22/17/45/leaf-1001679_960_720.jpg",
-      name: "Steven",
-      link: "https://pixabay.com/photos/leaf-green-foliage-green-leaves-1001679/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Men/1969.jpg?raw=true",
+      type: "men",
       size: "medium",
     },
     {
       id: 17,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2018/05/16/22/27/rose-3407234_960_720.jpg",
-      name: "Rosee",
-      link: "https://pixabay.com/photos/rose-red-drops-rain-macro-nature-3407234/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Men/1977.jpg?raw=true",
+      type: "men",
       size: "medium",
     },
     {
       id: 18,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2018/01/29/19/00/park-3116883_960_720.jpg",
-      name: "Arcoin",
-      link: "https://pixabay.com/photos/park-bench-night-evening-fog-mist-3116883/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Men/1978.jpg?raw=true",
+      type: "men",
       size: "large",
     },
     {
       id: 19,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/11/22/23/44/porsche-1851246_960_720.jpg",
-      name: "Pexel",
-      link: "https://pixabay.com/photos/porsche-car-brake-lights-1851246/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Men/1980.jpg?raw=true",
+      type: "men",
       size: "small",
     },
     {
       id: 20,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/06/05/20/41/girl-1438138_960_720.jpg",
-      name: "Xusenru",
-      link: "https://pixabay.com/photos/girl-umbrella-rain-park-autumn-1438138/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Men/1985.jpg?raw=true",
+      type: "men",
       size: "large",
     },
     {
       id: 21,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2017/03/16/20/20/man-2150164_960_720.jpg",
-      name: "Fifalinana",
-      link: "https://pixabay.com/photos/man-waterfalls-bathe-bathing-falls-2150164/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Women/1789.jpg?raw=true",
+      type: "women",
       size: "large",
     },
     {
       id: 22,
       imgSrc:
         "https://cdn.pixabay.com/photo/2016/11/29/11/12/rain-1869119_960_720.jpg",
-      name: "Pexel",
-      link: "https://pixabay.com/photos/rain-woman-brunette-portrait-1869119/",
+      type: "women",
       size: "medium",
     },
     {
       id: 23,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/11/19/11/11/hands-1838659_960_720.jpg",
-      name: "Pexel",
-      link: "https://pixabay.com/photos/hands-plant-soil-grow-growing-1838659/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Women/1793.jpg?raw=true",
+      type: "women",
       size: "medium",
     },
     {
       id: 24,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2019/01/25/11/18/girl-3954232_960_720.jpg",
-      name: "Pexel",
-      link: "https://pixabay.com/photos/girl-rain-femininity-beauty-paris-3954232/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Women/1796.jpg?raw=true",
+      type: "women",
       size: "large",
     },
     {
       id: 25,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2013/02/20/10/32/snail-83672_960_720.jpg",
-      name: "Larisa",
-      link: "https://pixabay.com/illustrations/snail-drops-rain-raindrops-mollusk-83672/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Women/1800.jpg?raw=true",
+      type: "women",
       size: "samll",
     },
     {
       id: 26,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2020/08/30/09/28/buildings-5528981_960_720.jpg",
-      name: "China Town",
-      link: "https://pixabay.com/photos/buildings-houses-street-city-rain-5528981/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Women/1802.jpg?raw=true",
+      type: "women",
       size: "small",
     },
     {
       id: 27,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2017/03/12/02/20/triangle-2136288_960_720.jpg",
-      name: "Pyramid",
-      link: "https://pixabay.com/illustrations/triangle-sky-abstract-wallpaper-2136288/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Women/1803.jpg?raw=true",
+      type: "women",
       size: "large",
     },
     {
       id: 28,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2020/04/02/23/01/rain-4996916_960_720.jpg",
-      name: "Pyou",
-      link: "https://pixabay.com/photos/rain-street-tables-cafe-flowers-4996916/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Women/1805.jpg?raw=true",
+      type: "women",
       size: "small",
     },
     {
       id: 29,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2020/05/19/13/05/heart-5190672_960_720.jpg",
-      name: "Heart",
-      link: "https://pixabay.com/photos/heart-window-rain-drops-love-5190672/",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Women/1806.jpg?raw=true",
+      type: "women",
       size: "small",
     },
     {
       id: 30,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg",
-      name: "cat",
-      link: "https://pixabay.com/photos/cat-kitten-pet-kitty-young-cat-551554/",
-      size: "large",
+        "https://github.com/shikhasurupa/StyleBoard/blob/main/Images/Women/1810.jpg?raw=true",
+      type: "women",
+      size: "small",
     },
   ];
   
