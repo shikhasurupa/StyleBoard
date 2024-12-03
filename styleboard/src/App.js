299db7de132bd14cd0bf1005e37765463fd386d1
Laksita <laksita.prasanna@gmail.com>
2024-12-02 20:30:57 -0500

    Replaced main screen with feed (For now)

--- styleboard/src/App.js
+++ styleboard/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginRegister from './Component/LoginRegister/LoginRegister';
+import Feed from './Component/Feed/FeedScreen';
 
 function App() {
   return (
     <div>
-      <LoginRegister/>
+      <Feed/>
     </div>
   );
 }
