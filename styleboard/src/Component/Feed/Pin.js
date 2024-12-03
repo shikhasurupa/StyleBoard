7ab4b5e4d09e2e66ea3ce8cfe1a5e2e4867fc46e
Laksita <laksita.prasanna@gmail.com>
2024-12-03 11:57:10 -0500

    removed unnecessary loc

--- styleboard/src/Component/Feed/Pin.js
+++ styleboard/src/Component/Feed/Pin.js
@@ -4,11 +4,7 @@ import React from "react";
 function Pin({pinSize, imgSrc}) {
     return (
         <div className={`pin ${pinSize}`}>
-            <img
-                className='mainPic' 
-                src={imgSrc}
-                alt=""
-            />
+            <img src={imgSrc}/>
         </div>
     );
 }
