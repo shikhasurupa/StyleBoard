6312eb62e8cd7e5d5279dfe7337c02fd9d1d8d4d
Laksita <laksita.prasanna@gmail.com>
2024-12-04 14:38:05 -0500

    Added filter button

--- styleboard/src/Component/Header/Header.jsx
+++ styleboard/src/Component/Header/Header.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import logo from "./logo.png";
-import "./Header.css"
+import "./Header.css";
 
-
-//Header
+// Header
 const Header = () => {
-    return (
-        <div className = "header">
-            <div className="logo-container">
-                <img src={logo} alt="StyleBoard Logo" className="logo-image" />
-            </div>
-            <div className="links">
-                <button className="link-button">Home</button>
-                <button className="link-button">Collections</button>
-                <button className="log-out-button"> Log Out</button>
-            </div>
-        </div>
-    )
-}
-  
-  export default Header;
+  const handleCategoryChange = (event) => {
+    const selectedCategory = event.target.value;
+    if (selectedCategory) {
+      console.log(`Selected category: ${selectedCategory}`);
+    }
+  };
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={logo} alt="StyleBoard Logo" className="logo-image" />
+      </div>
+      <div className="links">
+        <select
+            className="dropdown-select"
+            onChange={handleCategoryChange}
+            defaultValue="">
+            <option value="" disabled>Filters</option>
+            <option value="Women">Women</option>
+            <option value="Men">Men</option>
+            <option value="Kids">Kids</option>
+        </select>
+        <button className="link-button">Home</button>
+        <button className="link-button">Collections</button>
+        <button className="log-out-button">Log Out</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
