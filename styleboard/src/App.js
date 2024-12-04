6edd97b912ab8ab6549c0ec485dfb837c31b1505
Laksita <laksita.prasanna@gmail.com>
2024-12-03 23:22:52 -0500

    added route path for collection screen

--- styleboard/src/App.js
+++ styleboard/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import LoginRegister from './Component/LoginRegister/LoginRegister';
 import Feed from './Component/Feed/FeedScreen';
+import CollectionScreen from './Component/Collection/CollectionScreen';
 import { Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LoginRegister />} />
         <Route path='/feed' element={<Feed />} />
+        <Route path='/collection' element={<CollectionScreen />} />
       </Routes>
     </div>
   );
