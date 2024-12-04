1b44f2538ad5521808ecaa4a120013846149f2ea
Laksita <laksita.prasanna@gmail.com>
2024-12-03 23:23:24 -0500

    save button redirects to collection page for now

--- styleboard/src/Component/Feed/Pin.js
+++ styleboard/src/Component/Feed/Pin.js
@@ -1,12 +1,14 @@
 import './Pin.css';
 import React from "react";
+import { useNavigate } from 'react-router-dom'
 
 function Pin({pinSize, imgSrc}) {
+    const navigate = useNavigate();
     return (
         <div className={`pin ${pinSize}`}>
             <img src={imgSrc}/>
             <div>
-                <button className='save'>
+                <button className='save' onClick={() => navigate("/collection")}>
                     <img src="https://github.com/shikhasurupa/StyleBoard/blob/FeedScreen/styleboard/src/Images/OvelSaveButton.png?raw=true" />
                 </button>
             </div>
